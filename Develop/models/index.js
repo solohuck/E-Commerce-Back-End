067355395212a1c09b97de002adccad4ee050997
solo <solohuckstep@gmail.com>
2022-12-01 00:52:34 -0500

    added belongsTo and hasMany

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'category_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
